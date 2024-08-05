a8346de181405b1af62f0c5b7132d127a4640bbf
Parthvi <parthvi.vala@suse.com>
2024-08-05 17:46:49 +0530

    Automate Qase 183, 189, 190, 191, 267, 268, 269
    Signed-off-by: Parthvi <parthvi.vala@suse.com>

--- hosted/aks/p1/p1_suite_test.go
+++ hosted/aks/p1/p1_suite_test.go
@@ -1,6 +1,8 @@
 package p1_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -8,7 +10,9 @@ import (
 	. "github.com/rancher-sandbox/qase-ginkgo"
 	"github.com/rancher/shepherd/clients/rancher"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
 	namegen "github.com/rancher/shepherd/pkg/namegenerator"
+	"k8s.io/utils/pointer"
 
 	"github.com/rancher/hosted-providers-e2e/hosted/aks/helper"
 	"github.com/rancher/hosted-providers-e2e/hosted/helpers"
@@ -60,4 +64,106 @@ func updateAutoScaling(cluster *management.Cluster, client *rancher.Client) {
 		cluster, err = helper.UpdateAutoScaling(cluster, client, false, 0, 0, true)
 		Expect(err).To(BeNil())
 	})
+}
+
+func removeSystemNpCheck(cluster *management.Cluster, client *rancher.Client) {
+	updateFunc := func(cluster *management.Cluster) {
+		var upgradedNodePools []management.AKSNodePool
+		for _, np := range cluster.AKSConfig.NodePools {
+			if np.Mode == "User" {
+				upgradedNodePools = append(upgradedNodePools, np)
+			}
+		}
+		cluster.AKSConfig.NodePools = upgradedNodePools
+	}
+	var err error
+	cluster, err = helper.UpdateCluster(cluster, client, updateFunc)
+	Expect(err).To(BeNil())
+	Eventually(func() bool {
+		cluster, err = client.Management.Cluster.ByID(cluster.ID)
+		Expect(err).To(BeNil())
+		return cluster.Transitioning == "error" && strings.Contains(cluster.TransitioningMessage, "There has to be at least one system agent pool")
+	}, "5m", "5s").Should(BeTrue())
+}
+
+func deleteAndAddNpCheck(cluster *management.Cluster, client *rancher.Client) {
+	npToBeDeleted := cluster.AKSConfig.NodePools[0]
+	updateFunc := func(cluster *management.Cluster) {
+		upgradedNodePools := cluster.AKSConfig.NodePools[1:]
+
+		upgradedNodePools = append(upgradedNodePools, management.AKSNodePool{
+			AvailabilityZones:   npToBeDeleted.AvailabilityZones,
+			Count:               npToBeDeleted.Count,
+			EnableAutoScaling:   npToBeDeleted.EnableAutoScaling,
+			MaxCount:            npToBeDeleted.MaxCount,
+			MaxPods:             npToBeDeleted.MaxPods,
+			MaxSurge:            npToBeDeleted.MaxSurge,
+			MinCount:            npToBeDeleted.MinCount,
+			Mode:                "System",
+			Name:                pointer.String(fmt.Sprintf("newpool%s", namegen.RandStringLower(3))),
+			NodeLabels:          npToBeDeleted.NodeLabels,
+			NodeTaints:          npToBeDeleted.NodeTaints,
+			OrchestratorVersion: npToBeDeleted.OrchestratorVersion,
+			OsDiskSizeGB:        npToBeDeleted.OsDiskSizeGB,
+			OsDiskType:          npToBeDeleted.OsDiskType,
+			OsType:              npToBeDeleted.OsType,
+			VMSize:              npToBeDeleted.VMSize,
+			VnetSubnetID:        npToBeDeleted.VnetSubnetID,
+		})
+		cluster.AKSConfig.NodePools = upgradedNodePools
+	}
+	var err error
+	cluster, err = helper.UpdateCluster(cluster, client, updateFunc)
+	Expect(err).To(BeNil())
+	var (
+		npDeleted = true
+		npAdded   = false
+	)
+	for _, np := range cluster.AKSConfig.NodePools {
+		if *np.Name == *npToBeDeleted.Name {
+			npDeleted = false
+		}
+		if *np.Name == "newpool" {
+			npAdded = true
+		}
+	}
+	Expect(npAdded).To(BeTrue())
+	Expect(npDeleted).To(BeTrue())
+
+	err = clusters.WaitClusterToBeUpgraded(client, cluster.ID)
+	Expect(err).To(BeNil())
+
+	Eventually(func() bool {
+		cluster, err = client.Management.Cluster.ByID(cluster.ID)
+		Expect(err).To(BeNil())
+		var (
+			npDeletedFromUpstream = true
+			npAddedToUpstream     = false
+		)
+		for _, np := range cluster.AKSConfig.NodePools {
+			if *np.Name == "newpool" {
+				npAddedToUpstream = true
+			}
+			if *np.Name == *npToBeDeleted.Name {
+				npDeletedFromUpstream = false
+			}
+		}
+		return npAddedToUpstream && npDeletedFromUpstream
+	}, "5m", "5s").Should(BeTrue(), "Failed while waiting for node pools to be added and deleted")
+
+}
+
+// npUpgradeToVersionGTCPCheck runs checks when nodepool is upgraded to a version greater than control plane version
+func npUpgradeToVersionGTCPCheck(cluster *management.Cluster, client *rancher.Client) {
+	k8sVersion := cluster.AKSConfig.KubernetesVersion
+	availableVersions, err := helper.ListAKSAvailableVersions(client, cluster.ID)
+	Expect(err).To(BeNil())
+	upgradeK8sVersion := availableVersions[0]
+	cluster, err = helper.UpgradeNodeKubernetesVersion(cluster, upgradeK8sVersion, client, false, false)
+	Expect(err).To(BeNil())
+	Eventually(func() bool {
+		cluster, err = client.Management.Cluster.ByID(cluster.ID)
+		Expect(err).NotTo(HaveOccurred())
+		return cluster.Transitioning == "error" && strings.Contains(cluster.TransitioningMessage, fmt.Sprintf("Node pool version %s and control plane version %s are incompatible.", upgradeK8sVersion, k8sVersion))
+	}, "1m", "2s").Should(BeTrue())
 }
