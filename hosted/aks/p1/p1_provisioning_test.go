afefed9bda4f1c6c8c064b1e02145c884bbb5439
Parthvi <parthvi.vala@suse.com>
2024-08-09 16:15:12 +0530

    Remove focus
    Signed-off-by: Parthvi <parthvi.vala@suse.com>

--- hosted/aks/p1/p1_provisioning_test.go
+++ hosted/aks/p1/p1_provisioning_test.go
@@ -33,7 +33,7 @@ var _ = Describe("P1Provisioning", func() {
 		}
 	})
 
-	FIt("should successfully create cluster with multiple nodepools in multiple AZs", func() {
+	It("should successfully create cluster with multiple nodepools in multiple AZs", func() {
 		testCaseID = 193
 		updateFunc := func(aksConfig *aks.ClusterConfig) {
 			nodepools := *aksConfig.NodePools
@@ -74,7 +74,7 @@ var _ = Describe("P1Provisioning", func() {
 		}
 	})
 
-	FIt("should be able to create a cluster with empty tag", func() {
+	It("should be able to create a cluster with empty tag", func() {
 		testCaseID = 205
 		updateFunc := func(aksConfig *aks.ClusterConfig) {
 			aksConfig.Tags["empty-tag"] = ""
@@ -138,7 +138,7 @@ var _ = Describe("P1Provisioning", func() {
 			updateAutoScaling(cluster, ctx.RancherAdminClient)
 		})
 
-		FIt("should be able to update tags", func() {
+		It("should be able to update tags", func() {
 			testCaseID = 177
 			updateTagsCheck(cluster, ctx.RancherAdminClient)
 		})
