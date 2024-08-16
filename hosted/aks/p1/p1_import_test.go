f40b589030b36049b3ce43e058987a3267cff7f4
Parthvi Vala <parthvi.vala@suse.com>
2024-08-16 14:14:47 +0530

    Add focus
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- hosted/aks/p1/p1_import_test.go
+++ hosted/aks/p1/p1_import_test.go
@@ -52,7 +52,7 @@ var _ = Describe("P1Import", func() {
 			updateAutoScaling(cluster, ctx.RancherAdminClient)
 		})
 
-		It("should be able to update tags", func() {
+		FIt("should be able to update tags", func() {
 			testCaseID = 270
 			updateTagsCheck(cluster, ctx.RancherAdminClient)
 		})
