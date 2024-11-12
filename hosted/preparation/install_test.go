49de1f2bdc73c45493826b8fcbcfd9e2e2dd1f1f
Tomas Hehejik <thehejik@suse.com>
2024-11-12 11:47:11 +0100

    WIP run proxy in separate By block, wait for capi-controller-manager
    Signed-off-by: Tomas Hehejik <thehejik@suse.com>

--- hosted/preparation/install_test.go
+++ hosted/preparation/install_test.go
@@ -43,9 +43,9 @@ var _ = Describe("Provision k3s cluster with Rancher", Label("install"), func() 
 	//localKubeconfig := os.Getenv("HOME") + "/.kube/config"
 
 	It("Install upstream k3s cluster", func() {
-		By("Installing K3s", func() {
-			// Configure proxy before K3s installation if requested
-			if proxy != "" {
+		if proxy != "" {
+			By("Run local squid proxy in docker", func() {
+				// Configure proxy before K3s installation if requested
 				GinkgoLogr.Info("Starting squid proxy")
 
 				cwd, _ := os.Getwd()
@@ -56,19 +56,21 @@ var _ = Describe("Provision k3s cluster with Rancher", Label("install"), func() 
 					"-p", "3128:3128", "ubuntu/squid").CombinedOutput()
 				GinkgoWriter.Println(string(out))
 				Expect(err).To(Not(HaveOccurred()))
+			})
 
-				GinkgoLogr.Info("Setting up proxy for K3s installation")
-
+			By("Configure proxy in /etc/default/k3s", func() {
 				k3sConfigPath := "/etc/default/k3s"
 				k3sConfig := fmt.Sprintf(`HTTP_PROXY=%s
 HTTPS_PROXY=%s
 NO_PROXY=127.0.0.0/8,10.0.0.0/8,cattle-system.svc,172.16.0.0/12,192.168.0.0/16,.svc,.cluster.local`, proxyUrl, proxyUrl)
 				// Write the proxy configuration to the file as root
-				out, err = exec.Command("sh", "-c", fmt.Sprintf("echo '%s' | sudo tee %s", k3sConfig, k3sConfigPath)).CombinedOutput()
+				out, err := exec.Command("sh", "-c", fmt.Sprintf("echo '%s' | sudo tee %s", k3sConfig, k3sConfigPath)).CombinedOutput()
 				GinkgoWriter.Println(string(out))
 				Expect(err).To(Not(HaveOccurred()))
-			}
+			})
+		}
 
+		By("Installing K3s", func() {
 			// Get K3s installation script
 			fileName := "k3s-install.sh"
 			Eventually(func() error {
@@ -211,6 +213,27 @@ NO_PROXY=127.0.0.0/8,10.0.0.0/8,cattle-system.svc,172.16.0.0/12,192.168.0.0/16,.
 
 			checkList := [][]string{
 				{"cattle-system", "app=rancher-webhook"},
+			}
+			Eventually(func() error {
+				return rancher.CheckPod(k, checkList)
+			}, tools.SetTimeout(4*time.Minute), 30*time.Second).Should(BeNil())
+		})
+
+		By("Waiting for capi-controller-manager", func() {
+			// Wait unit the kubectl command returns exit code 0
+			count := 1
+			Eventually(func() error {
+				out, err := kubectl.Run("rollout", "status",
+					"--namespace", "cattle-provisioning-capi-system",
+					"deployment", "capi-controller-manager",
+				)
+				GinkgoWriter.Printf("Waiting for capi-controller-manager deployment, loop %d:\n%s\n", count, out)
+				count++
+				return err
+			}, tools.SetTimeout(2*time.Minute), 5*time.Second).Should(Not(HaveOccurred()))
+
+			checkList := [][]string{
+				{"cattle-provisioning-capi-system", "cluster.x-k8s.io/provider=cluster-api"},
 			}
 			Eventually(func() error {
 				return rancher.CheckPod(k, checkList)
