ed806edf8440d5350ff65e570d231c8bceb12047
Tomas Hehejik <thehejik@suse.com>
2024-12-18 17:27:53 +0100

    Make sure there is always at least 1s difference

--- hosted/gke/p0/p0_provisioning_test.go
+++ hosted/gke/p0/p0_provisioning_test.go
@@ -81,9 +81,11 @@ var _ = Describe("P0Provisioning", func() {
 				Expect(err).NotTo(HaveOccurred())
 				GinkgoLogr.Info(fmt.Sprintf("Using K8s version %s for cluster %s", k8sVersion, clusterName))
 
+				// Do not create clusters exactly at the same time for all ginkgo nodes
 				nodeIndex := GinkgoParallelNode()
-				randomDelay := time.Duration((nodeIndex%3)*1000+rand.Intn(1000)) * time.Millisecond
+				randomDelay := time.Duration((nodeIndex%3+1)*1000+rand.Intn(1000)) * time.Millisecond
 				time.Sleep(randomDelay)
+
 				cluster, err = helper.CreateGKEHostedCluster(ctx.RancherAdminClient, clusterName, ctx.CloudCredID, k8sVersion, zone, region, project, updateFunc)
 				Expect(err).To(BeNil())
 				cluster, err = helpers.WaitUntilClusterIsReady(cluster, ctx.RancherAdminClient)
