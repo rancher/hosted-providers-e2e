5d187772cb77a7f44ae8a7003127fe89cf0496fd
Parthvi Vala <parthvi.vala@suse.com>
2025-01-23 14:09:02 +0530

    Add issue ref
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- hosted/eks/p1/p1_provisioning_test.go
+++ hosted/eks/p1/p1_provisioning_test.go
@@ -170,6 +170,7 @@ var _ = Describe("P1Provisioning", func() {
 	})
 
 	It("Deploy a cluster with Public/Priv access then disable Public access", func() {
+		// https://github.com/rancher/eks-operator/issues/752#issuecomment-2609144199
 		testCaseID = 151
 		createFunc := func(clusterConfig *eks.ClusterConfig) {
 			clusterConfig.PublicAccess = pointer.Bool(true)
