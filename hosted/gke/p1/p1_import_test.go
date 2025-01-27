06dc4dbca2eed6f68f4b0a913e332c8a1ee7bfee
Parthvi Vala <parthvi.vala@suse.com>
2025-01-27 19:06:03 +0530

    Add focus
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- hosted/gke/p1/p1_import_test.go
+++ hosted/gke/p1/p1_import_test.go
@@ -43,7 +43,7 @@ var _ = Describe("P1Import", func() {
 			Expect(err).To(BeNil())
 		})
 
-		It("User should not be able to import a cluster using an expired GKE creds", func() {
+		FIt("User should not be able to import a cluster using an expired GKE creds", func() {
 			testCaseID = 305
 			By("adding the creds")
 			cloudCredentialConfig := cloudcredentials.CloudCredential{GoogleCredentialConfig: &cloudcredentials.GoogleCredentialConfig{AuthEncodedJSON: os.Getenv("SECONDARY_GCP_CREDENTIALS")}}
@@ -71,7 +71,7 @@ var _ = Describe("P1Import", func() {
 			}, "2m", "3s").Should(BeTrue())
 		})
 
-		It("User should not be able to import cluster with invalid GKE creds in Rancher", func() {
+		FIt("User should not be able to import cluster with invalid GKE creds in Rancher", func() {
 			testCaseID = 306
 
 			By("creating invalid creds")
